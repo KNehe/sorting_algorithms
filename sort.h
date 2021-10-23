50c409b5ba5989b8c17eb0ae78cd6eeba905d7a6
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2021-10-23 13:05:13 +0000

    insertion sort

--- sort.h
+++ sort.h
@@ -18,4 +18,6 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void insertion_swap(listint_t *n1, listint_t *n2, listint_t **head);
 #endif

518950eb7ce0c6c9ecc114bced5ce74bd7a37ddc
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2021-10-23 14:31:14 +0000

    selection sort

--- sort.h
+++ sort.h
@@ -20,4 +20,5 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void insertion_swap(listint_t *n1, listint_t *n2, listint_t **head);
+void selection_sort(int *array, size_t size);
 #endif

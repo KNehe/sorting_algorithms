602e6d8aa9f15cfbad2f9014349e64519a399d68
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2021-10-24 12:01:34 +0000

    quick sort

--- sort.h
+++ sort.h
@@ -21,4 +21,9 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void insertion_swap(listint_t *n1, listint_t *n2, listint_t **head);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void swap(int *num1, int *num2);
+size_t partition_array(int *arr, size_t low, size_t pt, size_t size);
+void qSort_helper(int *arr, size_t low, size_t high, size_t size);
+
 #endif
